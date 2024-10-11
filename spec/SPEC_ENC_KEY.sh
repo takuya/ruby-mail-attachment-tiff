b70a4f756801770ed958b7a33de516eba3138572
takuya <55338+takuya@users.noreply.github.com>
2024-10-11 15:39:32 +0900

    update-filename

--- spec/SPEC_ENC_KEY.sh
+++ spec/SPEC_ENC_KEY.sh
@@ -1,3 +1,3 @@
 
-openssl enc -e -aes-256-cbc -pbkdf2 -iter 10000000 -base64 -in SPEC_ENC_KEY.txt -out SPEC_ENC_KEY.txt.encrypted
-openssl enc -d -aes-256-cbc -pbkdf2 -iter 10000000 -base64 -in SPEC_ENC_KEY.txt.encrypted -out SPEC_ENC_KEY.txt
+openssl enc -e -aes-256-cbc -pbkdf2 -iter 10000000 -base64 -in SPEC_ENC_KEY.txt -out SPEC_ENC_KEY.txt.enc
+openssl enc -d -aes-256-cbc -pbkdf2 -iter 10000000 -base64 -in SPEC_ENC_KEY.txt.enc -out SPEC_ENC_KEY.txt
